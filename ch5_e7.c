4d37b5e8f209e872a9944fcf6a1a01cffd8c85ca
bluej15 <bluej15@users.noreply.github.com>
2015-07-19 07:27:22 -0400

    Create ch5_e7.c

---
+++ ch5_e7.c
@@ -0,0 +1,27 @@
+// Print prime numbers up to 50
+
+#include <stdio.h>
+
+int main (void)
+{
+    int p, d;
+    _Bool isPrime;
+    
+    printf("2 ");
+    
+    for(p = 3; p <=50; p+=2)
+    {
+        isPrime = 1;
+        
+        for(d = 3; d < p && isPrime != 0; d+=2)
+            if(p % d == 0)
+                isPrime = 0;
+                
+            if(isPrime != 0)
+                printf("%i ", p);
+    }
+
+    printf("\n");
+    
+    return 0;
+}
