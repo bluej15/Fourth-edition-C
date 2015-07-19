20744bd1cb84ebc905b6903092f8e2e628e2da80
bluej15 <bluej15@users.noreply.github.com>
2015-07-19 07:17:28 -0400

    Create ch4_e8.c

---
+++ ch4_e8.c
@@ -0,0 +1,26 @@
+// User input triangular numbers
+
+#include <stdio.h>
+
+int main (void)
+{
+    int n, number, triangularNumber, count, i;
+    
+    printf("How many traingular number do you want?\n");
+    scanf("%i", &count);
+    
+    for(i = 0; i < count; i++)
+    {
+        printf("What triangular number do you want?\n");
+        scanf("%i", &number);
+        
+        triangularNumber = 0;
+        
+        for(n = 1; n <= number; n++)
+            triangularNumber += n;
+            
+        printf("Triangular number %i is %i\n", number, triangularNumber);
+    }
+
+    return 0;
+}
